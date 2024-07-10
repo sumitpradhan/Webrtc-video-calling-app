c74b20df2c00273e5ecfddf8d2adfb7e85656ec6
Sumit Pradhan <60561782+sumitpradhan@users.noreply.github.com>
2024-07-10 16:14:38 +0530

    Update index.js
    Fixed Audio Bug

--- components/BottomTools/index.js
+++ components/BottomTools/index.js
@@ -5,7 +5,7 @@ const BottomTools=({muted,playing,handleAudioToggle,handleVideoToggle,leaveRoom}
 
     return(
         <div className="absolute w-[300px] flex gap-2 bottom-5 left-0 right-0 mx-auto">
-            {muted?<Mic 
+            {!muted?<Mic 
             className=" rounded-full cursor-pointer "
             onClick={handleAudioToggle}/>:<MicOff 
             className=" bg-red-400 rounded-full cursor-pointer"
